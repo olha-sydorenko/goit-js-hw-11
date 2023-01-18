8e148e861db6ecee7291b41958d74ca41df2d7ff
olha-sydorenko <108460682+olha-sydorenko@users.noreply.github.com>
2023-01-18 04:23:19 +0200

    B4 axios

---
+++ src/PixabayAPI.js
@@ -0,0 +1,18 @@
+
+export class PixabayAPI {
+
+constructor() {
+    this.page = 1;
+    this.query = null;
+  }
+
+searchPhotos() {
+    return fetch(`https://pixabay.com/api/?key=32917411-0bf5fafbdbcee2600446b2252&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=40`)
+.then(response => {
+if (!response.ok) {
+    throw new Error(response.status);
+}
+ return response.json();
+});
+}
+}
